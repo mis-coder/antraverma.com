1cf73e2af955a4c60eb2dc7f28ef382dee7f6f70
Antra Verma <antraverma99@gmail.com>
2024-05-09 15:39:00 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>
+        Thank you for visiting my site. Stay tuned! Something amazing coming
+        soon 🎉{" "}
+      </h1>
+      <p>- Antra Verma</p>
+    </main>
+  );
+}
