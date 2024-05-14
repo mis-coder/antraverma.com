26626fb09953e980a80091e7fa8060dea61afb97
Antra Verma <antraverma99@gmail.com>
2024-05-14 11:32:07 +0530

    updated footer style

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
   return (
-    <footer>
-      Made with passion 🔥 by Antra. All rights reserved © Antra Verma 2024
+    <footer className="flex flex-col w-full h-fit p-4 items-center">
+      <p>Made with passion 🔥 by Antra.</p>
+      <p className="text-xs"> All rights reserved © Antra Verma 2024</p>
     </footer>
   );
 };
