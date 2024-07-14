14a1d0e4ffa81b9961584b7ebc0686337e4c1064
Antra Verma <antraverma99@gmail.com>
2024-07-14 10:26:25 +0530

    made improvements to about page

--- app/(pages)/about/page.jsx
+++ app/(pages)/about/page.jsx
@@ -1,33 +1,31 @@
 import Image from "next/image";
-import TopImg from "../../assets/images/website-about-pic-1.jpeg";
+
+import ProfileImg from "@/app/assets/images/website-about-pic-1.jpeg";
 
 const AboutPage = () => {
   return (
-    <section className="pt-20">
-      <div className="flex w-full h-full gap-20 items-center justify-center">
+    <section className="pt-5 pb-20 md:pt-20 md:pb-0">
+      <div className="flex flex-col-reverse md:flex-row w-full h-full gap-10 md:gap-20 items-center justify-center">
         <div className="w-full md:w-1/2">
-          <h2 className="font-barlow text-3xl md:text-5xl">Hey it's Antra!</h2>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum
+          <h2 className="font-barlow text-4xl md:text-5xl">Hey it's Antra!</h2>
+          <p className="mt-4 text-sm md:text-md">
+            I am a 25-year-old Software Developer based in Mohali, India,
+            currently working with Authenticate® as a Full Stack Developer. With
+            a robust network of over 6.5K connections on LinkedIn, I am
+            passionate about problem-solving and simplifying complex challenges.
+            <br />
+            <br /> I value kindness, empathy, and the pursuit of making the
+            world a better place. I love my family and strive to integrate these
+            values into both my professional and personal life.
           </p>
         </div>
-        <div className="">
-          <Image
-            src={TopImg}
-            quality={80}
-            alt="my-picture"
-            className="w-96 h-96 z-10 object-cover rounded-3xl"
-          />
-          <div />
-        </div>
+        <Image
+          src={ProfileImg}
+          quality={80}
+          alt="my-picture"
+          className="md:w-96 h-96 object-cover rounded-3xl"
+          placeholder="blur"
+        />
       </div>
     </section>
   );
