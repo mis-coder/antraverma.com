1b342fe93dcdd23c8eab48bfc13fa83e5765e7d2
Antra Verma <antraverma99@gmail.com>
2024-08-18 10:49:28 +0530

    added github link

--- app/constants/static-data/index.ts
+++ app/constants/static-data/index.ts
@@ -19,4 +19,9 @@ export const navItems = [
     title: "LinkedIn",
     url: "https://www.linkedin.com/in/antra-verma/",
   },
+  {
+    target: "_blank",
+    title: "Github",
+    url: "https://github.com/mis-coder/",
+  },
 ];
